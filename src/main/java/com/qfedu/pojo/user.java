b6a722e596edac9281a0bf56aa45463376c1c10c
lcl <943947943@qq.com>
2019-06-17 19:44:49 +0800

    登录功能

--- src/main/java/com/qfedu/pojo/user.java
+++ src/main/java/com/qfedu/pojo/user.java
@@ -6,5 +6,69 @@ package com.qfedu.pojo;/**
  * @author HP liuchenglong
  * @create 2019-06-13 17:28
  **/
-public class user {
+public class User {
+
+    private Integer id;
+    private String no;
+    private String password;
+    private String name;
+    private Integer role;
+    private Integer flag;
+    private String headphoto;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNo() {
+        return no;
+    }
+
+    public void setNo(String no) {
+        this.no = no;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getRole() {
+        return role;
+    }
+
+    public void setRole(Integer role) {
+        this.role = role;
+    }
+
+    public Integer getFlag() {
+        return flag;
+    }
+
+    public void setFlag(Integer flag) {
+        this.flag = flag;
+    }
+
+    public String getHeadphoto() {
+        return headphoto;
+    }
+
+    public void setHeadphoto(String headphoto) {
+        this.headphoto = headphoto;
+    }
 }
